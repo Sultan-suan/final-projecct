7f5ee5f29de8531600d26307fd63193b9a1c0d69
Sultan <107674919+Sultan-suan@users.noreply.github.com>
2023-03-29 01:20:36 +0600

    Создал 3 переменных для Email, Password, Submit. В кнопку передал значение данных для каждого

--- src/components/Registration/Registration.tsx
+++ src/components/Registration/Registration.tsx
@@ -1,18 +1,54 @@
-import React from 'react';
-import s from './Registration.module.css'
+import React, { useState } from 'react';
+import s from './Registration.module.css';
 
 const Registration = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEmailChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+        setEmail(event.target.value);
+    };
+
+    const handlePasswordChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+        setPassword(event.target.value);
+    };
+
+    const handleSubmit = (event: { preventDefault: () => void; }) => {
+        event.preventDefault();
+        console.log(`Email: ${email}, Password: ${password}`);
+
+    };
+
     return (
         <div className={s.wrapper}>
-          <div>
-              <h4>Sign in</h4>
-          </div>
             <div>
-                <input/>
-                <input/>
+                <h4>Registration</h4>
             </div>
-            <button className={s.button}> Sign in</button>
-
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={handleEmailChange}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                        required
+                    />
+                </div>
+                <button type="submit" className={s.button}>
+                    Register now
+                </button>
+            </form>
         </div>
     );
 };
