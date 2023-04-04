dc1062722bd921bacc2409889b5bec4efa8a11fc
Nakysbek Ernar <Nakysbek.e@mail.ru>
2023-04-04 22:33:03 +0600

    new update

--- src/Route/AppRouter.tsx
+++ src/Route/AppRouter.tsx
@@ -2,17 +2,13 @@ import React, {useState} from 'react';
 import {Route, Routes} from "react-router-dom";
 import {privateRoutes, publicRoutes} from "./Routes";
 import {Login} from "../components/Login/Login";
-import {AuthProvider} from "../AuthProvider";
+
 
 export const AppRouter = () => {
 
-    // const isAuth = false;
     const [isAuth, setIsAuth] = useState(false);
 
-    // Вопрос. Как можно передать в компоненту состояние в данном случае?
-
     return (
-        <AuthProvider>
             <Routes>
                 {isAuth && privateRoutes.map(({path, element}) =>
                     <Route key={path}
@@ -26,7 +22,6 @@ export const AppRouter = () => {
                 )}
                 <Route path='/*' element=<Login/> />
             </Routes>
-        </AuthProvider>
     )
 };
 
